7202e8c84e2dea0dada3fdda42b30f043f7915ac
aKuad <53811809+aKuad@users.noreply.github.com>
2025-11-03 22:35:34 +0900

    Add: JSDoc missing at a private function in `atom.ts`
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/feed/atom.ts
+++ src/feed/atom.ts
@@ -134,16 +134,24 @@ export async function fetch_atom(atom_url: string): Promise<Post[]> {
 }
 
 
-function to_absolute_when_relative(may_relative_url: string, feed_url: string): string {
+/**
+ * Convert a path to absolute URI - If already absolute, return original URI
+ * @private
+ *
+ * @param may_relative_uri URI string to convert
+ * @param feed_uri URI origin of `may_relative_url`
+ * @returns Absolute URI
+ */
+function to_absolute_when_relative(may_relative_uri: string, feed_uri: string): string {
   try {
     // When `Invalid URL` not thrown, it was absolute URL
-    new URL(may_relative_url);
-    return may_relative_url;
+    new URL(may_relative_uri);
+    return may_relative_uri;
 
   } catch(_) {
     // When thrown, it was relative URL
-    const url = new URL(feed_url);
-    url.pathname = may_relative_url;
+    const url = new URL(feed_uri);
+    url.pathname = may_relative_uri;
     return url.href;
   }
 }
