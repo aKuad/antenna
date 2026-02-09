a5692d7072ac9524faf11af9d89999733da2f7b5
aKuad <53811809+aKuad@users.noreply.github.com>
2026-02-09 22:24:55 +0900

    Add: New tests for error output checking
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

---
+++ src/tests/atom.test.ts
@@ -0,0 +1,171 @@
+/**
+ * Posts fetching tests from Atom feed
+ */
+
+import { serveDir } from "jsr:@std/http@1";
+import { assertEquals } from "jsr:@std/assert@1";
+
+import { sleep } from "../util.ts";
+
+import { fetch_posts } from "../fetch_posts.ts";
+import { Feed, FetchResult } from "../types.ts";
+
+
+Deno.test(async function test_atom(t) {
+  /* Test HTTP server start */
+  const test_server = Deno.serve(async request => {
+    if(request.headers.get("no-icon-test") === "true" && new URL(request.url).pathname === "/favicon.ico")
+      return new Response("Not Found", { status: 404, statusText: "Not Found" });
+
+    const response_delay_ms = request.headers.get("late-response-test");
+    if(response_delay_ms)
+      await new Promise(resolve => setTimeout(resolve, Number(response_delay_ms)));
+
+    return serveDir(request, { fsRoot: "./test_data/atom", urlRoot: "", quiet: true });
+  });
+
+
+  /**
+   * - Can fetch and return posts data from specified URL
+   */
+  await t.step(async function atom_true() {
+    const posts_general_actual      = await fetch_posts([{ type: "atom", url: "http://localhost:8000/true_general.xml" }]);
+    const posts_email_scheme_actual = await fetch_posts([{ type: "atom", url: "http://localhost:8000/true_email_scheme.xml" }]);
+    const posts_minimum_actual      = await fetch_posts([{ type: "atom", url: "http://localhost:8000/true_minimum.xml", headers: { "no-icon-test": "true" } }]);
+
+    const posts_general_expected: FetchResult = {
+      posts: [
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/icon.ico",
+          title: "Fully case entry",
+          url: "http://example.com/entry",
+          author_name: "Fully case entry author",
+          author_url: "http://example.com/author/entry-author",
+          author_icon_url: undefined,
+          description: "Entry summary",
+          thumbnail_url: "http://localhost:8000/logo.png",
+          post_date: new Date("2025-11-01T00:00:00Z"),
+          update_date: new Date("2025-11-01T00:01:00Z")
+        },
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/icon.ico",
+          title: "Minimum case entry",
+          url: "http://example.com/",
+          author_name: "General author",
+          author_url: "http://example.com/author/general-author",
+          author_icon_url: undefined,
+          description: "General subtitle",
+          thumbnail_url: "http://localhost:8000/logo.png",
+          post_date: undefined,
+          update_date: new Date("2025-11-01T01:00:00Z")
+        }
+      ],
+      fail_reasons: []
+    };
+
+    const posts_email_scheme_expected: FetchResult = {
+      posts: [
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/favicon.ico",
+          title: "Author specified entry",
+          url: "http://localhost:8000",
+          author_name: "Entry author",
+          author_url: "mailto:entry-author@example.com",
+          author_icon_url: undefined,
+          description: undefined,
+          thumbnail_url: undefined,
+          post_date: undefined,
+          update_date: new Date("2025-11-01T20:01:00Z")
+        },
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/favicon.ico",
+          title: "Minimum case entry",
+          url: "http://localhost:8000",
+          author_name: "General author",
+          author_url: "mailto:general-author@example.com",
+          author_icon_url: undefined,
+          description: undefined,
+          thumbnail_url: undefined,
+          post_date: undefined,
+          update_date: new Date("2025-11-01T20:02:00Z")
+        }
+      ],
+      fail_reasons: []
+    };
+
+    const posts_minimum_expected: FetchResult = {
+      posts: [
+        {
+          site_name: "localhost",
+          site_icon_url: undefined,
+          title: "Minimum case entry",
+          url: "http://localhost:8000",
+          author_name: "localhost",
+          author_url: undefined,
+          author_icon_url: undefined,
+          description: undefined,
+          thumbnail_url: undefined,
+          post_date: undefined,
+          update_date: new Date("2025-11-01T10:01:00Z")
+        }
+      ],
+      fail_reasons: []
+    };
+
+    assertEquals(posts_general_actual     , posts_general_expected);
+    assertEquals(posts_email_scheme_actual, posts_email_scheme_expected);
+    assertEquals(posts_minimum_actual     , posts_minimum_expected);
+  });
+
+
+  /**
+   * - When invalid URL passed, returns empty array with error message logging
+   * - When invalid file as atom fetched, returns empty array with error message logging
+   */
+  await t.step(async function atom_err() {
+    const invalid_url_target: Feed = { type: "atom", url: "non_url_string" };
+    const timeout_target    : Feed = { type: "atom", url: "http://localhost:8000/true_general.xml", timeout_ms: 100, headers: { "late-response-test": "500" } };
+    const not_found_target  : Feed = { type: "atom", url: "http://localhost:8000/non_existing_file" };
+    const non_xml_target    : Feed = { type: "atom", url: "http://localhost:8000/err_non_xml.xml" };
+    const no_feed_target    : Feed = { type: "atom", url: "http://localhost:8000/err_no_feed.xml" };
+    const no_entries_target : Feed = { type: "atom", url: "http://localhost:8000/err_no_entries.xml" };
+    const no_title_target   : Feed = { type: "atom", url: "http://localhost:8000/err_no_title.xml" };
+    const no_updated_target : Feed = { type: "atom", url: "http://localhost:8000/err_no_updated.xml" };
+
+    const invalid_url_actual = fetch_posts([invalid_url_target]);
+    const timeout_actual     = fetch_posts([timeout_target]);
+    const not_found_actual   = fetch_posts([not_found_target]);
+    const non_xml_actual     = fetch_posts([non_xml_target]);
+    const no_feed_actual     = fetch_posts([no_feed_target]);
+    const no_entries_actual  = fetch_posts([no_entries_target]);
+    const no_title_actual    = fetch_posts([no_title_target]);
+    const no_updated_actual  = fetch_posts([no_updated_target]);
+
+    const invalid_url_expected: FetchResult = { posts: [], fail_reasons: [{ target: invalid_url_target, severity: "error"  , category: "FetchParamError", detail: "TypeError: Invalid URL: 'non_url_string'" }]};
+    const timeout_expected    : FetchResult = { posts: [], fail_reasons: [{ target: timeout_target    , severity: "error"  , category: "TimeoutError"   , detail: "Request timeout" }]};
+    const not_found_expected  : FetchResult = { posts: [], fail_reasons: [{ target: not_found_target  , severity: "error"  , category: "HTTPError"      , detail: "Not Found" }]};
+    const non_xml_expected    : FetchResult = { posts: [], fail_reasons: [{ target: non_xml_target    , severity: "error"  , category: "ParseError"     , detail: "SyntaxError: Malformed XML document: empty document or no root node detected" }]};
+    const no_feed_expected    : FetchResult = { posts: [], fail_reasons: [{ target: no_feed_target    , severity: "error"  , category: "DataMissing"    , detail: "<feed> tag required, but not found" }]};
+    const no_entries_expected : FetchResult = { posts: [], fail_reasons: [{ target: no_entries_target , severity: "warning", category: "DataMissing"    , detail: "No <entry> tags found, no posts fetched" }]};
+    const no_title_expected   : FetchResult = { posts: [], fail_reasons: [{ target: no_title_target   , severity: "error"  , category: "DataMissing"    , detail: "At an <entry> tag index 0, <title> tag required, but not found" }]};
+    const no_updated_expected : FetchResult = { posts: [], fail_reasons: [{ target: no_updated_target , severity: "error"  , category: "DataMissing"    , detail: "At an <entry> tag index 0, <updated> tag required, but not found" }]};
+
+    assertEquals(await invalid_url_actual, invalid_url_expected);
+    assertEquals(await timeout_actual    , timeout_expected);
+    assertEquals(await not_found_actual  , not_found_expected);
+    assertEquals(await non_xml_actual    , non_xml_expected);
+    assertEquals(await no_feed_actual    , no_feed_expected);
+    assertEquals(await no_entries_actual , no_entries_expected);
+    assertEquals(await no_title_actual   , no_title_expected);
+    assertEquals(await no_updated_actual , no_updated_expected);
+  });
+
+
+  /* Test HTTP server shutdown */
+  await sleep(100); // Interval for fetch() completed before shutdown
+  await test_server.shutdown();
+});
---
+++ src/tests/rss.test.ts
@@ -0,0 +1,214 @@
+/**
+ * Posts fetching tests from RSS feed
+ */
+
+import { serveDir } from "jsr:@std/http@1";
+import { assertEquals } from "jsr:@std/assert@1";
+
+import { sleep } from "../util.ts";
+
+import { fetch_posts } from "../fetch_posts.ts";
+import { Feed, FetchResult } from "../types.ts";
+
+
+Deno.test(async function test_rss(t) {
+  /* Test HTTP server start */
+  const test_server = Deno.serve(async request => {
+    if(request.headers.get("no-icon-test") === "true" && new URL(request.url).pathname === "/favicon.ico")
+      return new Response("Not Found", { status: 404, statusText: "Not Found" });
+
+    const response_delay_ms = request.headers.get("late-response-test");
+    if(response_delay_ms)
+      await new Promise(resolve => setTimeout(resolve, Number(response_delay_ms)));
+
+    return serveDir(request, { fsRoot: "./test_data/rss", urlRoot: "", quiet: true })
+  });
+
+
+  /**
+   * - Can fetch and return posts data from specified URL
+   */
+  await t.step(async function rss_true() {
+    const posts_general_actual   = await fetch_posts([{ type: "rss", url: "http://localhost:8000/true_general.xml" }]);
+    const posts_webMaster_actual = await fetch_posts([{ type: "rss", url: "http://localhost:8000/true_webMaster.xml" }]);
+    const date_at_webMaster_test = new Date();
+    const posts_copyright_actual = await fetch_posts([{ type: "rss", url: "http://localhost:8000/true_copyright.xml" }]);
+    const date_at_copyright_test = new Date();
+    const posts_minimum_actual   = await fetch_posts([{ type: "rss", url: "http://localhost:8000/true_minimum.xml", headers: { "no-icon-test": "true" } }]);
+    const date_at_minimum_test   = new Date();
+
+    const posts_general_expected: FetchResult = {
+      posts: [
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/favicon.ico",
+          title: "Fully case item - title",
+          url: "http://example.com/articles/item1",
+          author_name: "Item author",
+          author_url: "mailto:item-author@example.com",
+          author_icon_url: undefined,
+          description: "Fully case item - description",
+          thumbnail_url: "http://example.com/item-enclosure.png",
+          post_date: new Date("Thu, 04 Dec 2025 01:00:00 GMT"),
+          update_date: new Date("Thu, 04 Dec 2025 01:00:00 GMT")
+        },
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/favicon.ico",
+          title: "Non image enclosure item - title",
+          url: "http://example.com/articles",
+          author_name: "Managing Editor",
+          author_url: "mailto:managing-editor@example.com",
+          author_icon_url: undefined,
+          description: "General description",
+          thumbnail_url: "http://example.com/channel-image.png",
+          post_date: new Date("Thu, 04 Dec 2025 12:00:00 GMT"),
+          update_date: new Date("Thu, 04 Dec 2025 12:00:00 GMT")
+        },
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/favicon.ico",
+          title: "Minimum case item - title",
+          url: "http://example.com/articles",
+          author_name: "Managing Editor",
+          author_url: "mailto:managing-editor@example.com",
+          author_icon_url: undefined,
+          description: "General description",
+          thumbnail_url: "http://example.com/channel-image.png",
+          post_date: new Date("Thu, 04 Dec 2025 12:00:00 GMT"),
+          update_date: new Date("Thu, 04 Dec 2025 12:00:00 GMT")
+        },
+        {
+          site_name: "localhost",
+          site_icon_url: "http://localhost:8000/favicon.ico",
+          title: "General title",
+          url: "http://example.com/articles",
+          author_name: "Managing Editor",
+          author_url: "mailto:managing-editor@example.com",
+          author_icon_url: undefined,
+          description: "Minimum case item - description",
+          thumbnail_url: "http://example.com/channel-image.png",
+          post_date: new Date("Thu, 04 Dec 2025 12:00:00 GMT"),
+          update_date: new Date("Thu, 04 Dec 2025 12:00:00 GMT")
+        }
+      ],
+      fail_reasons: []
+    };
+
+    const posts_webMaster_expected: FetchResult = {
+      posts: [{
+        site_name: "localhost",
+        site_icon_url: "http://localhost:8000/favicon.ico",
+        title: "webMaster test case item - title",
+        url: "http://example.com/articles",
+        author_name: "Web Master",
+        author_url: "mailto:web-master@example.com",
+        author_icon_url: undefined,
+        description: "General description",
+        thumbnail_url: undefined,
+        post_date: undefined,
+        update_date: date_at_webMaster_test
+      }],
+      fail_reasons: []
+    };
+
+    const posts_copyright_expected: FetchResult = {
+      posts: [{
+        site_name: "localhost",
+        site_icon_url: "http://localhost:8000/favicon.ico",
+        title: "copyright test case item - title",
+        url: "http://example.com/articles",
+        author_name: "Copyright 2025, aKuad",
+        author_url: undefined,
+        author_icon_url: undefined,
+        description: "General description",
+        thumbnail_url: undefined,
+        post_date: undefined,
+        update_date: date_at_copyright_test
+      }],
+      fail_reasons: []
+    };
+
+    const posts_minimum_expected: FetchResult = {
+      posts: [{
+        site_name: "localhost",
+        site_icon_url: undefined,
+        title: "Minimum case item - title",
+        url: "http://example.com/articles",
+        author_name: "localhost",
+        author_url: undefined,
+        author_icon_url: undefined,
+        description: "General description",
+        thumbnail_url: undefined,
+        post_date: undefined,
+        update_date: date_at_minimum_test
+      }],
+      fail_reasons: []
+    };
+
+    assertEquals(posts_general_actual,   posts_general_expected);
+    assertEquals(posts_webMaster_actual, posts_webMaster_expected);
+    assertEquals(posts_copyright_actual, posts_copyright_expected);
+    assertEquals(posts_minimum_actual,   posts_minimum_expected);
+  });
+
+
+  /**
+   * - When invalid URL passed, returns empty array with error message logging
+   * - When invalid file as RSS fetched, returns empty array with error message logging
+   */
+  await t.step(async function rss_err() {
+    const invalid_url_target   : Feed = { type: "rss", url: "non_url_string" };
+    const timeout_target       : Feed = { type: "rss", url: "http://localhost:8000/true_general.xml", timeout_ms: 100, headers: { "late-response-test": "500" } };
+    const not_found_target     : Feed = { type: "rss", url: "http://localhost:8000/non_existing_file" };
+    const non_xml_target       : Feed = { type: "rss", url: "http://localhost:8000/err_non_xml.xml" };
+    const no_rss_target        : Feed = { type: "rss", url: "http://localhost:8000/err_no_rss.xml" };
+    const no_channel_target    : Feed = { type: "rss", url: "http://localhost:8000/err_no_channel.xml" };
+    const no_title_target      : Feed = { type: "rss", url: "http://localhost:8000/err_no_title.xml" };
+    const no_link_target       : Feed = { type: "rss", url: "http://localhost:8000/err_no_link.xml" };
+    const no_description_target: Feed = { type: "rss", url: "http://localhost:8000/err_no_description.xml" };
+    const no_items_target      : Feed = { type: "rss", url: "http://localhost:8000/err_no_items.xml" };
+    const empty_item_target    : Feed = { type: "rss", url: "http://localhost:8000/err_empty_item.xml" };
+
+    const invalid_url_actual    = fetch_posts([invalid_url_target]);
+    const timeout_actual        = fetch_posts([timeout_target]);
+    const not_found_actual      = fetch_posts([not_found_target]);
+    const non_xml_actual        = fetch_posts([non_xml_target]);
+    const no_rss_actual         = fetch_posts([no_rss_target]);
+    const no_channel_actual     = fetch_posts([no_channel_target]);
+    const no_title_actual       = fetch_posts([no_title_target]);
+    const no_link_actual        = fetch_posts([no_link_target]);
+    const no_description_actual = fetch_posts([no_description_target]);
+    const no_items_actual       = fetch_posts([no_items_target]);
+    const empty_item_actual     = fetch_posts([empty_item_target]);
+
+    const invalid_url_expected   : FetchResult = { posts: [], fail_reasons: [{ target: invalid_url_target   , severity: "error"  , category: "FetchParamError", detail: "TypeError: Invalid URL: 'non_url_string'" }]};
+    const timeout_expected       : FetchResult = { posts: [], fail_reasons: [{ target: timeout_target       , severity: "error"  , category: "TimeoutError"   , detail: "Request timeout" }]};
+    const not_found_expected     : FetchResult = { posts: [], fail_reasons: [{ target: not_found_target     , severity: "error"  , category: "HTTPError"      , detail: "Not Found" }]};
+    const non_xml_expected       : FetchResult = { posts: [], fail_reasons: [{ target: non_xml_target       , severity: "error"  , category: "ParseError"     , detail: "SyntaxError: Malformed XML document: empty document or no root node detected" }]};
+    const no_rss_expected        : FetchResult = { posts: [], fail_reasons: [{ target: no_rss_target        , severity: "error"  , category: "DataMissing"    , detail: "<rss> tag required, but not found" }]};
+    const no_channel_expected    : FetchResult = { posts: [], fail_reasons: [{ target: no_channel_target    , severity: "error"  , category: "DataMissing"    , detail: "<channel> tag required, but not found" }]};
+    const no_title_expected      : FetchResult = { posts: [], fail_reasons: [{ target: no_title_target      , severity: "error"  , category: "DataMissing"    , detail: "At <channel> tag, <title> tag required, but not found" }]};
+    const no_link_expected       : FetchResult = { posts: [], fail_reasons: [{ target: no_link_target       , severity: "error"  , category: "DataMissing"    , detail: "At <channel> tag, <link> tag required, but not found" }]};
+    const no_description_expected: FetchResult = { posts: [], fail_reasons: [{ target: no_description_target, severity: "error"  , category: "DataMissing"    , detail: "At <channel> tag, <description> tag required, but not found" }]};
+    const no_items_expected      : FetchResult = { posts: [], fail_reasons: [{ target: no_items_target      , severity: "warning", category: "DataMissing"    , detail: "No <item> tags found, no posts fetched" }]};
+    const empty_item_expected    : FetchResult = { posts: [], fail_reasons: [{ target: empty_item_target    , severity: "error"  , category: "DataMissing"    , detail: "At an <item> tag index 0, <title> or <description> tag required, but not found" }]};
+
+    assertEquals(await invalid_url_actual   , invalid_url_expected);
+    assertEquals(await timeout_actual       , timeout_expected);
+    assertEquals(await not_found_actual     , not_found_expected);
+    assertEquals(await non_xml_actual       , non_xml_expected);
+    assertEquals(await no_rss_actual        , no_rss_expected);
+    assertEquals(await no_channel_actual    , no_channel_expected);
+    assertEquals(await no_title_actual      , no_title_expected);
+    assertEquals(await no_link_actual       , no_link_expected);
+    assertEquals(await no_description_actual, no_description_expected);
+    assertEquals(await no_items_actual      , no_items_expected);
+    assertEquals(await empty_item_actual    , empty_item_expected);
+  });
+
+
+  /* Test HTTP server shutdown */
+  await sleep(100); // Interval for fetch() completed before shutdown
+  await test_server.shutdown();
+});
