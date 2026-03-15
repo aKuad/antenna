ec5fa036321352921f9f90e71fed145190b4a752
aKuad <53811809+aKuad@users.noreply.github.com>
2026-03-15 23:04:09 +0900

    Add: Error case tests for `fetch_qiita()`
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/tests/qiita.test.ts
+++ src/tests/qiita.test.ts
@@ -48,9 +48,32 @@ Deno.test(async function test_rss(t) {
 
 
   /**
-   * -
+   * - When response reached timeout duration, returns "TimeoutError" fail reason
+   * - When non exist ID specified, returns "DataMissing" fail reason
+   * - When rate limit exceeded, returns "HTTPError" fail reason
+   * - When server error occurred, returns "HTTPError" fail reason
    */
-  await t.step(async function qiita_err() {});
+  await t.step(async function qiita_err() {
+    const timeout_target       : SiteTarget = { site_name: "qiita", uid: "uid", timeout_ms: 100, headers: { "late-response-test": "500" } };
+    const uid_non_exists_target: SiteTarget = { site_name: "qiita", uid: "non-exist" };
+    const rate_limit_target    : SiteTarget = { site_name: "qiita", uid: "uid", headers: { "rate-limit-exceeded-test": "true" } };
+    const server_error_target  : SiteTarget = { site_name: "qiita", uid: "uid", headers: { "500-error-test": "true" } };
+
+    const timeout_actual        = fetch_posts([timeout_target], undefined);
+    const uid_non_exists_actual = fetch_posts([uid_non_exists_target], undefined, true);
+    const rate_limit_actual     = fetch_posts([rate_limit_target], undefined, true);
+    const server_error_actual   = fetch_posts([server_error_target], undefined, true);
+
+    const timeout_expected       : FetchResult = { posts: [], fail_reasons: [{ target: timeout_target       , severity: "error"  , category: "TimeoutError", detail: "Request timeout" }]};
+    const uid_non_exists_expected: FetchResult = { posts: [], fail_reasons: [{ target: uid_non_exists_target, severity: "warning", category: "DataMissing" , detail: "User ID non exist or no posted" }]};
+    const rate_limit_expected    : FetchResult = { posts: [], fail_reasons: [{ target: rate_limit_target    , severity: "error"  , category: "HTTPError"   , detail: "Rate limit exceeded" }]};
+    const server_error_expected  : FetchResult = { posts: [], fail_reasons: [{ target: server_error_target  , severity: "error"  , category: "HTTPError"   , detail: "Internal Server Error" }]};
+
+    assertEquals(await timeout_actual       , timeout_expected);
+    assertEquals(await uid_non_exists_actual, uid_non_exists_expected);
+    assertEquals(await rate_limit_actual    , rate_limit_expected);
+    assertEquals(await server_error_actual  , server_error_expected);
+  });
 
 
   /* Test HTTP server shutdown */
