345152f62ad82bcf889d03e0f04527be7e90dbf6
aKuad <53811809+aKuad@users.noreply.github.com>
2026-03-17 22:26:09 +0900

    Add: True case tests for `fetch_qiita()`
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/tests/qiita.test.ts
+++ src/tests/qiita.test.ts
@@ -30,7 +30,7 @@ Deno.test(async function test_rss(t) {
 
     if(url.searchParams.get("query") !== "user:uid") {
       const res = await serveFile(request, "./test_data/qiita/err_uid_non_exists.json");
-      res.headers.append("total-count", "101");
+      res.headers.append("total-count", "101"); // Actual item count on test is 4, but it for paging test
       return res;
     }
 
@@ -44,7 +44,67 @@ Deno.test(async function test_rss(t) {
   /**
    * - Can fetch and return posts data from specified user ID
    */
-  await t.step(async function qiita_true() {});
+  await t.step(async function qiita_true() {
+    const posts_actual = await fetch_posts([{ site_name: "qiita", uid: "uid" }], undefined, true);
+
+    const posts_expected: FetchResult = {
+      posts: [{
+        site_name: "Qiita",
+        site_icon_url: "https://qiita.com/favicon.ico",
+        title: "GitHub Actions 上で PlatformIO を素早くセットアップしてビルドチェックする",
+        url: "https://qiita.com/aKuad/items/c797897dbda98f8f86ec",
+        author_name: "aKuad",
+        author_url: "https://qiita.com/aKuad",
+        author_icon_url: "https://avatars2.githubusercontent.com/u/53811809?v=4",
+        description: "## きっかけ  [個人開発](https://github.com/aKuad/web-dmx)にて、[PlatformIO](https://platformio.org/)[^pio] でマイコ",
+        thumbnail_url: "",
+        post_date: new Date("2025-12-22T13:20:00.000Z"),
+        update_date: new Date("2025-12-30T14:07:21.000Z"),
+      },
+      {
+        site_name: "Qiita",
+        site_icon_url: "https://qiita.com/favicon.ico",
+        title: "GitHub Actions バージョンアプデ自動化めちゃ楽だった 〜はじめての Dependabot〜",
+        url: "https://qiita.com/aKuad/items/afd4585e7479dc4319f0",
+        author_name: "aKuad",
+        author_url: "https://qiita.com/aKuad",
+        author_icon_url: "https://avatars2.githubusercontent.com/u/53811809?v=4",
+        description: "## ある日  [あるリポジトリ](https://github.com/aKuad/docs-gen-samples)を久々に更新して、そのついでにワークフローで使用している Action のバージ",
+        thumbnail_url: "",
+        post_date: new Date("2026-01-11T06:19:08.000Z"),
+        update_date: new Date("2026-01-11T06:19:08.000Z"),
+      },
+      {
+        site_name: "Qiita",
+        site_icon_url: "https://qiita.com/favicon.ico",
+        title: "〜プレビューデプロイを求めて〜 GitHub Actions から Cloudflare Pages へデプロイ",
+        url: "https://qiita.com/aKuad/items/86c9b6774329f58f7695",
+        author_name: "aKuad",
+        author_url: "https://qiita.com/aKuad",
+        author_icon_url: "https://avatars2.githubusercontent.com/u/53811809?v=4",
+        description: "## 取り扱っているのは  こちら、Doxygen などソースコードドキュメントジェネレータのサンプル集てきなものを作っています。  https://github.com/aKuad/docs-gen",
+        thumbnail_url: "",
+        post_date: new Date("2026-01-29T13:30:01.000Z"),
+        update_date: new Date("2026-01-29T13:53:49.000Z"),
+      },
+      {
+        site_name: "Qiita",
+        site_icon_url: "https://qiita.com/favicon.ico",
+        title: "GitHub リポジトリの 'Deployments' に表示させるには？",
+        url: "https://qiita.com/aKuad/items/9d1cb399b2846f61648d",
+        author_name: "aKuad",
+        author_url: "https://qiita.com/aKuad",
+        author_icon_url: "https://avatars2.githubusercontent.com/u/53811809?v=4",
+        description: "## GitHub リポジトリの 'Deployments' って？  これのことです。  ![GitHub Repositoy - Deployments](https://qiita-image-",
+        thumbnail_url: "",
+        post_date: new Date("2026-02-13T12:25:53.000Z"),
+        update_date: new Date("2026-03-09T12:53:20.000Z"),
+      }],
+      fail_reasons: []
+    };
+
+    assertEquals(posts_actual, posts_expected);
+  });
 
 
   /**
